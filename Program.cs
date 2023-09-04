77c13864ae643654fe62f15777d35f0546ae608b
Isaac Scheff <isaac1996@comcast.net>
2023-09-04 17:38:07 -0400

    Added handling for multiple input types as well as input validation.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿const string gameName = "~The Game~";
+const string invalidEntry = "Selection not understood. Please type either the number or title of your selection and press Enter.";
 bool exitGame = false;
 bool inIntro = true;
 string? playerInput;
+string playerSelection;
 
 GameState gameState = new GameState();
 
@@ -9,7 +11,7 @@ GameState gameState = new GameState();
 //this loop runs the game
 do{
     if(inIntro){
-        Console.WriteLine($"Welcome to {gameName}");
+        Console.WriteLine($"\t\tWelcome to {gameName}\n\rPress Enter to continue");
         playerInput = Console.ReadLine();
         CreateCharacter();
     }
@@ -18,42 +20,48 @@ do{
 
 void CreateCharacter(){
     Console.WriteLine("\n\rSelect your chracter race:");
-    Console.WriteLine("1. Abarimon ~description here~");
-    Console.WriteLine("2. Faun ~description here~");
-    Console.WriteLine("3. Harpy ~description here~");
-    Console.WriteLine("4. Human ~description here~");
-    Console.WriteLine("5. Onocentaur ~description here~");
+    Console.WriteLine("1. Abarimon: ~description here~");
+    Console.WriteLine("2. Faun: ~description here~");
+    Console.WriteLine("3. Harpy: ~description here~");
+    Console.WriteLine("4. Human: ~description here~");
+    Console.WriteLine("5. Onocentaur: ~description here~");
 
-    playerInput = Console.ReadLine();
+    bool raceSelected = false;
+    while(raceSelected == false){
+        playerSelection = PlayerSelection(Console.ReadLine());
 
-    switch(playerInput){    //might need to loop all inputs for valid options
-        case "1":
-            gameState.playerRaceType = GameState.PlayerRace.Abarimon;
-            gameState.playerStats[0] += 1; //increase in agility
-            gameState.playerStats[4] -= 1; //decrease in vitality
-            break;
-        case "2":
-            gameState.playerRaceType = GameState.PlayerRace.Faun;
-            gameState.playerStats[4] += 1; //increase in vitality
-            gameState.playerStats[3] -= 1; //decrease in intuition
-            break;
-        case "3":
-            gameState.playerRaceType = GameState.PlayerRace.Harpy;
-            gameState.playerStats[1] += 1; //increase in brain-smarts
-            gameState.playerStats[2] -= 1; //decrease in brawn
-            break;
-        case "4":
-            gameState.playerRaceType = GameState.PlayerRace.Human;
-            gameState.playerStats[3] += 1; //increase in intuition
-            gameState.playerStats[0] -= 1; //decrease in agility
-            break;
-        case "5":
-            gameState.playerRaceType = GameState.PlayerRace.Onocentaur;
-            gameState.playerStats[2] += 1; //increase in brawn
-            gameState.playerStats[1] -= 1; //decrease in brain-smarts
-            break;
-        default:
-            break;
+        raceSelected = true;
+        switch(playerSelection){   
+            case "1": case "abarimon": 
+                gameState.playerRaceType = GameState.PlayerRace.Abarimon;
+                gameState.playerStats[0] += 1; //increase in agility
+                gameState.playerStats[4] -= 1; //decrease in vitality
+                break;
+            case "2": case "faun":
+                gameState.playerRaceType = GameState.PlayerRace.Faun;
+                gameState.playerStats[4] += 1; //increase in vitality
+                gameState.playerStats[3] -= 1; //decrease in intuition
+                break;
+            case "3": case "harpy":
+                gameState.playerRaceType = GameState.PlayerRace.Harpy;
+                gameState.playerStats[1] += 1; //increase in brain-smarts
+                gameState.playerStats[2] -= 1; //decrease in brawn
+                break;
+            case "4": case "human":
+                gameState.playerRaceType = GameState.PlayerRace.Human;
+                gameState.playerStats[3] += 1; //increase in intuition
+                gameState.playerStats[0] -= 1; //decrease in agility
+                break;
+            case "5": case "onocentaur":
+                gameState.playerRaceType = GameState.PlayerRace.Onocentaur;
+                gameState.playerStats[2] += 1; //increase in brawn
+                gameState.playerStats[1] -= 1; //decrease in brain-smarts
+                break;
+            default:
+                Console.WriteLine(invalidEntry);
+                raceSelected = false;
+                break;
+        }
     }
 
     Console.WriteLine("\n\rSelect your chracter class:");
@@ -63,31 +71,37 @@ void CreateCharacter(){
     Console.WriteLine("4. Murmillo ~description here~");
     Console.WriteLine("5. Retiarius ~description here~");
 
-    playerInput = Console.ReadLine();
+    bool classSelected = false;
+    while(classSelected == false){
+        playerSelection = PlayerSelection(Console.ReadLine());
 
-    switch(playerInput){    //might need to loop all inputs for valid options
-        case "1":
-            gameState.playerClassType = GameState.PlayerClass.Bestiarius;
-            gameState.playerStats[1] += 2; //increase in brain-smarts
-            break;
-        case "2":
-            gameState.playerClassType = GameState.PlayerClass.Cestus;
-            gameState.playerStats[2] += 2; //increase in brawn
-            break;
-        case "3":
-            gameState.playerClassType = GameState.PlayerClass.Dimachaerus;
-            gameState.playerStats[3] += 2; //increase in intuition
-            break;
-        case "4":
-            gameState.playerClassType = GameState.PlayerClass.Murmillo;
-            gameState.playerStats[4] += 2; //increase in vitality
-            break;
-        case "5":
-            gameState.playerClassType = GameState.PlayerClass.Retiarius;
-            gameState.playerStats[0] += 2; //increase in agility
-            break;
-        default:
-            break;
+        classSelected = true;
+        switch(playerSelection){    
+            case "1": case "bestiatius":
+                gameState.playerClassType = GameState.PlayerClass.Bestiarius;
+                gameState.playerStats[1] += 2; //increase in brain-smarts
+                break;
+            case "2": case "cestus":
+                gameState.playerClassType = GameState.PlayerClass.Cestus;
+                gameState.playerStats[2] += 2; //increase in brawn
+                break;
+            case "3": case "dimachaerus":
+                gameState.playerClassType = GameState.PlayerClass.Dimachaerus;
+                gameState.playerStats[3] += 2; //increase in intuition
+                break;
+            case "4": case "murmillo":
+                gameState.playerClassType = GameState.PlayerClass.Murmillo;
+                gameState.playerStats[4] += 2; //increase in vitality
+                break;
+            case "5": case "retiatius":
+                gameState.playerClassType = GameState.PlayerClass.Retiarius;
+                gameState.playerStats[0] += 2; //increase in agility
+                break;
+            default:
+                Console.WriteLine(invalidEntry);
+                classSelected = false;
+                break;
+        }
     }
     Console.WriteLine($"\n\rYou have selected a {gameState.playerRaceType} {gameState.playerClassType}.");
     Console.Write($"Agility: {gameState.playerStats[0]}   ");
@@ -95,6 +109,15 @@ void CreateCharacter(){
     Console.Write($"Brawn: {gameState.playerStats[2]}   ");
     Console.Write($"Intuition: {gameState.playerStats[3]}   ");
     Console.WriteLine($"Vitality: {gameState.playerStats[4]}");
+}
+
+string PlayerSelection(string? playerInput){
+    if (playerInput != null){
+            playerSelection = playerInput.ToLower();
+    } else {
+        playerSelection = "No selection";
+    }
+    return playerSelection;
 }
 
 public class GameState
